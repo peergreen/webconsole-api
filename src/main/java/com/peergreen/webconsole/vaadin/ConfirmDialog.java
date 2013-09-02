6d985cc5c8af1a16dd03fd17c94a734f5897c754
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-09-02 14:57:27 +0200

    Use Peergreen WebConsole default window

--- src/main/java/com/peergreen/webconsole/vaadin/ConfirmDialog.java
+++ src/main/java/com/peergreen/webconsole/vaadin/ConfirmDialog.java
@@ -2,21 +2,21 @@ package com.peergreen.webconsole.vaadin;
 
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
 
 import java.io.Serializable;
 
 /**
  * @author Mohammed Boukada
  */
-public class ConfirmDialog extends Window {
+public class ConfirmDialog extends DefaultWindow {
+
+    public ConfirmDialog(String caption, Component content, Button... footerButtons) {
+        super(caption, content, footerButtons);
+    }
 
     public interface Listener extends Serializable {
         void onClose(boolean isConfirmed);
@@ -55,41 +55,19 @@ public class ConfirmDialog extends Window {
         protected static final String DEFAULT_CANCEL_CAPTION = "Cancel";
 
         public static ConfirmDialog create(String windowCaption, Component content, String okCaption, String cancelCaption) {
-            final ConfirmDialog confirmDialog = new ConfirmDialog();
-            confirmDialog.setCaption(windowCaption != null ? windowCaption : DEFAULT_CAPTION);
-            confirmDialog.setClosable(false);
-            confirmDialog.setResizable(false);
-            confirmDialog.addStyleName("edit-dashboard");
-
-            VerticalLayout main = new VerticalLayout();
-            main.setSpacing(true);
-            main.setMargin(true);
-            main.setStyleName("confirm-dialog");
-
             if (content == null) {
                 content = new Label(DEFAULT_MESSAGE, ContentMode.HTML);
             }
-            content.addStyleName("confirm-dialog-message");
-            main.addComponent(content);
-
-            HorizontalLayout buttons = new HorizontalLayout();
-            buttons.setSizeFull();
-            buttons.setSpacing(true);
-            buttons.setMargin(true);
-            buttons.addStyleName("footer");
-            buttons.setWidth("100%");
 
             Button cancel = new Button(cancelCaption != null ? cancelCaption : DEFAULT_CANCEL_CAPTION);
             cancel.setClickShortcut(ShortcutAction.KeyCode.ESCAPE, null);
-            buttons.addComponent(cancel);
-            buttons.setExpandRatio(cancel, 1);
-            buttons.setComponentAlignment(cancel, Alignment.TOP_RIGHT);
 
             final Button ok = new Button(okCaption != null ? okCaption : DEFAULT_OK_CAPTION);
             ok.addStyleName("wide");
             ok.addStyleName("default");
             ok.setClickShortcut(ShortcutAction.KeyCode.ENTER, null);
-            buttons.addComponent(ok);
+
+            final ConfirmDialog confirmDialog = new ConfirmDialog(windowCaption != null ? windowCaption : DEFAULT_CAPTION, content, cancel, ok);
 
             Button.ClickListener clickListener = new Button.ClickListener() {
                 @Override
@@ -107,9 +85,6 @@ public class ConfirmDialog extends Window {
             };
             cancel.addClickListener(clickListener);
             ok.addClickListener(clickListener);
-            main.addComponent(buttons);
-
-            confirmDialog.setContent(main);
             return confirmDialog;
         }
     }
