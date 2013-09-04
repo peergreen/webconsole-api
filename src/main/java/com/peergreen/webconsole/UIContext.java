51a39443073a5b763f44dc5b2c3114f24e6bf7c1
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-09-04 10:58:49 +0200

    javadoc

--- src/main/java/com/peergreen/webconsole/UIContext.java
+++ src/main/java/com/peergreen/webconsole/UIContext.java
@@ -4,15 +4,32 @@ import com.peergreen.webconsole.navigator.ViewNavigator;
 import com.vaadin.ui.UI;
 
 /**
+ * UI context
  * @author Mohammed Boukada
  */
 public interface UIContext {
 
+    /**
+     * Get Web Console security manager
+     * @return security manager
+     */
     ISecurityManager getSecurityManager();
 
+    /**
+     * Get Vaadin UI
+     * @return Vaadin UI
+     */
     UI getUI();
 
+    /**
+     * Get Vaadin view navigator
+     * @return view navigator
+     */
     ViewNavigator getViewNavigator();
 
+    /**
+     * Get UI id
+     * @return UI id
+     */
     String getUIId();
 }
--- src/main/java/com/peergreen/webconsole/navigator/NavigationContext.java
+++ src/main/java/com/peergreen/webconsole/navigator/NavigationContext.java
@@ -7,7 +7,22 @@ import java.util.Map;
  * @author Mohammed Boukada
  */
 public interface NavigationContext {
+
+    /**
+     * Get current path
+     * @return current path
+     */
     String getPath();
+
+    /**
+     * Set path
+     * @param path path
+     */
     void setPath(String path);
+
+    /**
+     * Get navigation properties
+     * @return navigation properties
+     */
     Map<String, Object> getProperties();
 }
