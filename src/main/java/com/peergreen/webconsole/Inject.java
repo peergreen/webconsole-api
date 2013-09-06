97864126b2122df76d499bcc4fa6269c4133e361
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-09-06 11:03:52 +0200

    @Inject can be added to a field or a setter method

--- src/main/java/com/peergreen/webconsole/Inject.java
+++ src/main/java/com/peergreen/webconsole/Inject.java
@@ -6,9 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * This annotated can be added to a field or a setter method
  * @author Mohammed Boukada
  */
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Inject {
 }
